70dd6ebea1aa0f086b3a8aa253ffa003eb9fee97
Devadutta Hati <devhati83@gmail.com>
2021-12-20 14:57:30 +0530

    small changes

--- mergesort.cpp
+++ mergesort.cpp
@@ -5,21 +5,57 @@ int a[N];
 
 void merge(int l, int r, int mid)
 {
-    int l_size = mid - l + 1;
-    int L[l_size + 1];
-    int r_size = r - mid;
-    int R[r_size];
-    for (int i = 0; i < l_size; i++)
+    int l_sz = mid - l + 1;
+    int L[l_sz + 1];    // left array construction // +1 for INT_MAX approach
+    int r_sz = r - mid; //r-(mid+1) +1
+    int R[r_sz + 1];    // right array constuction
+    for (int i = 0; i < l_sz; i++)
     {
-        L[i] = a[i + 1];
+        L[i] = a[i + l];
     }
+    for (int i = 0; i < r_sz; i++)
+    {
+        R[i] = a[i + mid + 1];
+    }
+
+    L[l_sz] = R[r_sz] = INT_MAX;
+    int l_i = 0;
+    int r_i = 0;
+    for (int i = 0; i <= r; i++)
+    {
+        if (L[l_i] <= R[r_i])    //left array construction
+        {
+            a[i] = L[l_i];
+            l_i++;
+        }
+        else
+        {
+            a[i] = R[r_i];    // right array construction
+            r_i++;
+        }
+    }
+}
+
+void mergeSort(int l, int r)
+{
+    if (l == r)
+        return;                    //mergesort function
+    int mid = (l + r) / 2;
+    mergeSort(l, mid);
+    mergeSort(mid + 1, r);
+    merge(l, r, mid);
 }
 int main()
 {
     int n;
     cin >> n;
-    int a[n];
     for (int i = 0; i < n; i++)
     {
+        cin >> a[i];
+    }                                         //main function
+    mergeSort(0, n - 1);
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
     }
 }
