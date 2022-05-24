e1583eadbcb12f58197c5bc7926e31567d60da63
Devadutta Hati <devhati83@gmail.com>
2022-05-24 17:37:22 +0530

    insertion sort implement

---
+++ sorting/insertionsort.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+const ll INF = 1e9 + 7;
+const ll MOD = 998244353;
+ 
+#define loop(i, a, n) for (ll i = a; i < n; i++)
+#define pb(x) push_back(x)
+#define nl '\n'
+#define sz size()
+#define fi first
+#define se second
+#define each(a,x) for (auto& a: x)
+#define all(x) (x).begin(),(x).end()
+#define debug(x) cout << #x << " " << x << endl;
+#define Vmax(x)   *max_element(all(x))
+#define Vmin(x)   *min_element(all(x))
+#define fastasfkboi  ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define fix(prec) \{\cout<<setprecision(prec)<<fixed;\}
+using namespace std;
+typedef long long ll;
+#define charToInt(c) (c-'0')
+
+int main()
+{
+ fastasfkboi
+  int t=1;
+  cin >> t;
+  while(t--){
+  	//start here//
+
+  	
+    
+  }
+return 0;
+}
